6867f953aa3d8c007cd331c0f6869fb5a196ab3d
Leta <stdev17@gmail.com>
2021-12-28 11:19:29 +0900

    fix: GA script v10 - origin

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,17 +14,19 @@ echo 'Pushing it back to gh-pages repository...'
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 cd _site && \
 git init && \
+git config --global init.defaultBranch main && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git remote set-url origin $remote_repo && \
+git remote add origin $remote_repo && \
 echo "commiting" &&\
 git add . && \
 git commit -m 'action build - $(date)' > /dev/null 2>&1 && \
 echo "pushing" &&\
-git push --force origin master:gh-pages > /dev/null 2>&1 && \
-echo "done" && \
-rm -rf .git && \
-cd ..\
+git push --force -u origin main:gh-pages > /dev/null 2>&1 && \
+echo "done"
+
+rm -rf .git
+cd ..
 rm -rf _site
 
 echo 'Deploy job done'
