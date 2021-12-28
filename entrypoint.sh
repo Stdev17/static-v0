81d8588c06411f8e96bfcf2b01dcc47c151d2845
Leta <stdev17@gmail.com>
2021-12-29 04:39:54 +0900

    checkout

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,10 +19,11 @@ git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git remote add origin $remote_repo
 echo "commiting"
+git checkout --orphan gh-pages
 git add .
 git commit -m 'action build - $(date)'
 echo "pushing"
-git push --force -u origin main:gh-pages
+git push --force -u origin gh-pages
 
 rm -rf .git
 cd ..
