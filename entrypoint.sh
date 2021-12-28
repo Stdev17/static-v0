36eae0e74e3d9d6e9bf0f795abe5303fab7b6701
Leta <stdev17@gmail.com>
2021-12-28 10:44:20 +0900

    test: check envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,13 @@
 set -e
 
 echo 'Installing the gem bundle...'
-bundle install
+#bundle install
 
 echo 'Building the site...'
-bundle exec jekyll build
+#bundle exec jekyll build
 
-echo 'Pushing it back to gh-pages repository...'
-remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
-remote_branch="gh-pages" && \
-cd _site && \
-git init && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add . && \
-git commit -m 'action build - $(date)' > /dev/null 2>&1 && \
-git push --force $remote_repo main:$remote_branch > /dev/null 2>&1 && \
-rm -rf .git && \
-cd ..\
-rm -rf _site
+echo ${GITHUB_ACTOR} ${GITHUB_REPOSITORY}
+
+
 
 echo 'Deploy job done'
